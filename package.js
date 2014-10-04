7d0734e7ea0130e101b01c3e1227729e1ff51c1c
Paul Dowman <paul@pauldowman.com>
2014-10-04 14:23:28 -0400

    Bump version to 0.2.1 for bug fixes

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "u2622:persistent-session",
-  version: "0.2.0",
+  version: "0.2.1",
   summary: "Persistently store Session data on the client",
   git: "https://github.com/okgrow/meteor-persistent-session"
 });
