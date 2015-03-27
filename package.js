cb782633ff20bd334d46d37442937e58b6ba00cd
djhi <gildas.garcia@gmail.com>
2015-03-27 12:27:26 +0100

    add dependance on ejson package

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: "u2622:persistent-session",
-  version: "0.3.1",
+  version: "0.3.2",
   summary: "Persistently store Session data on the client",
   git: "https://github.com/okgrow/meteor-persistent-session"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('0.9.1'),
-  api.use(['jquery', 'amplify', 'session', 'underscore']);
+  api.use(['jquery', 'amplify', 'session', 'underscore', 'ejson']);
   api.export("PersistentSession");
   api.addFiles('lib/persistent_session.js', 'client');
 });
