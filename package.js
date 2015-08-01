d59c988244b28ce80bd8d59360fbecfe015a7e50
rgould <rwgould@gmail.com>
2015-07-31 19:53:50 -0700

    Release 0.4.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "u2622:persistent-session",
-  version: "0.3.4",
+  version: "0.4.0",
   summary: "Persistently store Session data on the client",
   git: "https://github.com/okgrow/meteor-persistent-session"
 });
