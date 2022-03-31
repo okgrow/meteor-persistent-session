91b6b04106957350259ccc506dcdca9996dc7b8d
Hung Vo <hung@dgroup.co>
2022-03-31 12:43:23 -0400

    use lodash instead of underscore because _.clone doesn't clone Session.set while lodash.cloneDeep() does

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
-  name: "u2622:persistent-session",
-  version: "0.4.4",
+  name: "designveloper:persistent-session",
+  version: "2.6.1",
   summary: "Persistently store Session data on the client",
-  git: "https://github.com/okgrow/meteor-persistent-session"
+  git: "https://github.com/Designveloper/meteor-persistent-session/"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('0.9.1'),
-  api.use(['jquery', 'amplify', 'tracker', 'reactive-dict', 'session', 'underscore', 'ejson']);
+  api.use(['jquery', 'amplify', 'tracker', 'reactive-dict', 'session', 'ejson', 'stevezhu:lodash']);
   // If `accounts-base` is loaded, we have to make sure that this package is
   // loaded after `accounts-base` is, so we specify `weak: true` here
-  api.use('accounts-base', { weak: true });
+  api.use('accounts-base@2.2.1', { weak: true });
   api.addFiles('lib/persistent_session.js', 'client');
   api.export('PersistentSession', ['client']);
 });
@@ -19,9 +19,9 @@ Package.onTest(function (api) {
   api.use("tinytest");
   api.use("amplify");
   api.use("random");
-  api.use("underscore");
+  api.use("stevezhu:lodash");
   api.use("reactive-dict"); // we only need this exposed for testing
-  api.use("u2622:persistent-session");
+  api.use("designveloper:persistent-session");
 
   // expose for derping around in console
   api.export('PersistentSession', ['client']);
